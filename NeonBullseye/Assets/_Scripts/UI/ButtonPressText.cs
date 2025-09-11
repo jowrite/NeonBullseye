d10957ae4f39fce4875c42e37b2e3a4e033272f4
jowrites <jodiekeene@outlook.com>
2025-09-11 13:29:29 -0400

    adding main menu with title & buttons

---
+++ NeonBullseye/Assets/_Scripts/UI/ButtonPressText.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ButtonPressText : MonoBehaviour
+{
+    [SerializeField] private RectTransform buttonTextRect;
+    private Vector3 originalPosition;
+
+    private void Start()
+    {
+        originalPosition = buttonTextRect.localPosition;
+    }
+
+    public void OnButtonPress()
+    {
+        buttonTextRect.localPosition = originalPosition + new Vector3(0, 3f, 0);
+    }
+
+    public void OnButtonRelease()
+    {
+        buttonTextRect.localPosition = originalPosition;
+    }
+}
